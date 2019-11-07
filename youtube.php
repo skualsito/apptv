9917cbda79d529e3a2fbadf030d5161943d9db07
Juan Almada <skual@live.com>
2019-11-07 10:17:59 -0300

    yt terminado

--- youtube.php
+++ youtube.php
@@ -14,6 +14,160 @@
     <title>Hello, world!</title>
   </head>
   <style>
+  .btn.focus, .btn:focus {
+    outline: 0;
+    box-shadow: none;
+  }
+  .app-tv-menu-general {
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 100%;
+      z-index: 9999;
+      background-color: #000;
+      -webkit-transition: all .3s ease-in-out;
+      -moz-transition: all .3s ease-in-out;
+      -o-transition: all .3s ease-in-out;
+      transition: all .3s ease-in-out;
+  }
+  .app-tv-menu-general.activo {
+      top: 0%;
+  }
+
+  .app-tv-menu-botonera {
+      width: 100%;
+      height: 100%;
+      padding: 20px;
+  }
+
+  .app-tv-menu-botonera .btn {
+      width: calc(50% - 20px);
+      height: 80px;
+      color: #fff;
+      background-color: #292929;
+      margin: 9px;
+  }
+
+  .app-tv-menu-botonera .btn i {
+      width: 100%;
+      font-size: 30px;
+      margin-top: 10px;
+  }
+
+  .app-tv-menu-botonera .btn span {
+      width: 100%;
+      font-size: 10px;
+      color: #8c8c8c;
+      text-transform: uppercase;
+      font-weight: 600;
+  }
+  .app-tv-menu-general.activo .app-tv-menu-footer {
+      bottom: 93%;
+      transform: rotate(180deg);
+  }
+  .app-tv-menu-footer {
+      position: fixed;
+      bottom: 0%;
+      left: calc(50% - 40px);
+      width: 80px;
+      height: 40px;
+      background-color: #000;
+      color: #fff;
+      font-size: 18px;
+      text-align: center;
+      line-height: 50px;
+      -moz-border-radius: 100px 100px 0 0;
+      -webkit-border-radius: 100px 100px 0 0;
+      border-radius: 100px 100px 0 0;
+      padding: 0;
+      -webkit-transition: all .3s ease-in-out;
+      -moz-transition: all .3s ease-in-out;
+      -o-transition: all .3s ease-in-out;
+      transition: all .3s ease-in-out;
+  }
+  .app-tv-menu-footer:hover {
+      color: #fff;
+  }
+  #cargador {
+      position: fixed;
+      z-index: -1;
+      background: #282828;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      opacity: 0;
+      -webkit-transition: all .5s ease-in-out;
+      -moz-transition: all .5s ease-in-out;
+      -o-transition: all .5s ease-in-out;
+      transition: all .5s ease-in-out;
+  }
+  #cargador.activo {
+    z-index: 9999;
+    opacity: 1;
+  }
+  #cargador .lds-ellipsis {
+      position: absolute;
+      left: calc(50% - 32px);
+      top: calc(50% - 32px);
+  }
+  .lds-ellipsis {
+    display: inline-block;
+    position: relative;
+    width: 64px;
+    height: 64px;
+  }
+  .lds-ellipsis div {
+    position: absolute;
+    top: 27px;
+    width: 11px;
+    height: 11px;
+    border-radius: 50%;
+    background: #fff;
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+  }
+  .lds-ellipsis div:nth-child(1) {
+    left: 6px;
+    animation: lds-ellipsis1 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(2) {
+    left: 6px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(3) {
+    left: 26px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(4) {
+    left: 45px;
+    animation: lds-ellipsis3 0.6s infinite;
+  }
+  @keyframes lds-ellipsis1 {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  @keyframes lds-ellipsis3 {
+    0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+  }
+  @keyframes lds-ellipsis2 {
+    0% {
+      transform: translate(0, 0);
+    }
+    100% {
+      transform: translate(19px, 0);
+    }
+  }
+
+  </style>
+  <style>
   @import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=swap');
   html, body {
     height: 100%;
@@ -129,22 +283,7 @@
       border-bottom-right-radius: 5px;
       width: 20%;
   }
-  .app-tv-menu-footer {
-      position: fixed;
-      bottom: 0;
-      left: calc(50% - 40px);
-      width: 80px;
-      height: 40px;
-      background-color: #000;
-      color: #fff;
-      font-size: 18px;
-      text-align: center;
-      line-height: 50px;
-      -moz-border-radius: 100px 100px 0 0;
-      -webkit-border-radius: 100px 100px 0 0;
-      border-radius: 100px 100px 0 0;
-      padding: 0;
-  }
+  
   .app-yt-control {
       width: 250px;
       height: 250px;
@@ -190,6 +329,7 @@
   }
   </style>
   <body class="app-yt">
+  <div id="cargador"><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>
     <div class="container">
       <div class="col-12">
         <div class="row app-yt-contenedor">
@@ -214,34 +354,48 @@
         </div>
       </div>
     </div>
-    <button class="btn app-tv-menu-footer">
-      <i class="fas fa-bars"></i>
-    </button>
+    <div class="app-tv-menu-general">
+      <button class="btn app-tv-menu-footer">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="app-tv-menu-botonera">
+        <button class="btn btn-home"><i class="fas fa-home"></i> <span>Home</span></button>
+      </div>
+    </div>
+    
+    
     <script>
-
       
-
+      
+      $(".btn-home").on("click", function () {
+        socket.emit('com-bg-app', 'index');
+      });
 
       const socket = io('http://11.11.15.8:8080');
       socket.emit('com-bg-app', 'obtener-recomendados-yt');
+      appCargador();
       $("#ytbuscador").on("input", function () {
         socket.emit('servidor-funcion',`document.getElementById("search-input").childNodes[0].value = "${$(this).val()}";`);
       });
 
       $(".btn-yt").on("click", function () {
         socket.emit('servidor-funcion',`document.getElementById("search-icon-legacy").click();`);
-        $('.app-yt-control').removeClass("activo");
-        $('.app-yt-videos').removeClass("activo").addClass("activo");
         $("#ytbuscador").val("");
         $(".app-yt-videos").scrollTop(0);
+        appCargador();
       });
 
-      $(".app-yt-logo").on("click", function () {
+      $(".app-yt-logo").on("click", function (e) {
+        e.preventDefault();
+        appCargador();
+        setTimeout(() => {
+          $('.app-yt-control').removeClass("activo");
+          $('.app-yt-videos').removeClass("activo").addClass("activo");
+        }, 500); 
         socket.emit('servidor-funcion',`location.href = "/";`); 
         $(".app-yt-videos").scrollTop(0);
-        socket.emit('com-bg-app', 'obtener-recomendados-yt');
-        $('.app-yt-control').removeClass("activo");
-        $('.app-yt-videos').removeClass("activo").addClass("activo");       
+        socket.emit('com-bg-app', 'obtener-recomendados-yt');  
+        
       });
 
       $(".app-yt-videos").on("scroll", function (e) {
@@ -255,19 +409,20 @@
         }
       });
 
-      $(document).on("click", ".app-yt-item img, .app-yt-item h4, .app-yt-item p", function (e) {
-        socket.emit('servidor-funcion',`location.href = "${$(this).closest(".app-yt-item").attr("href")}";`);
-        $('.app-yt-control').addClass("activo");
-        $('.app-yt-videos').removeClass("activo");
+      $(document).on("click", ".app-yt-item", function (e) {
+        socket.emit('servidor-funcion',`location.href = "${$(this).attr("href")}";`);
+        appCargador();
       });
 
-      $(document).on("click", ".app-yt-datos h5", function (e) {
-        socket.emit('servidor-funcion',`location.href = "${$(this).attr("href")}";`);
-      });
 
       $(".btn-up").on("click", function (e) {
         e.preventDefault();
         socket.emit('servidor-funcion',`window.history.back()`);
+        appCargador();
+        setTimeout(() => {
+          $('.app-yt-control').removeClass("activo");
+          $('.app-yt-videos').removeClass("activo").addClass("activo");
+        }, 500);        
       });
 
       $(".btn-down").on("click", function (e) {
@@ -291,14 +446,26 @@
         socket.emit('servidor-funcion',`location.href = $('.ytp-next-button').attr('href');`);
       });
 
-      socket.on('yt-on-client', function(data){        
+      $(".app-tv-menu-footer").on("click", function (e) {
+        e.preventDefault();
+        $(".app-tv-menu-general").toggleClass("activo");
+      });
+
+      socket.on('yt-on-client', function(data){   
         llenarGrilla(data[0].recomendados, true);
         $(".app-yt-user img").attr('src', data[0].usuario);
+        appCargador(false);     
+      });
+
+      socket.on('activar-control', function(data){        
+        $('.app-yt-control').removeClass("activo").addClass("activo");
+        $('.app-yt-videos').removeClass("activo");
+        appCargador(false);   
       });
       
       socket.on('resultados', function(data){
           llenarGrilla(data[0].recomendados);
-          
+          appCargador(false);
       });
       function llenarGrilla(data = [], r = false){
         if(r){
@@ -317,6 +484,15 @@
               </div>
               `);
           });
+      }
+
+      function appCargador(t = true) {
+
+        if(!t){
+          $("#cargador").removeClass("activo");
+        } else {
+          $("#cargador").addClass("activo");
+        }
       }
     </script>
   </body>
