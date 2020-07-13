968bae056011cc7ed8757b4f3a7a8bed9215d333
Juan Almada <skual@live.com>
2020-07-13 09:27:57 -0300

    prueba

---
+++ prueba.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/apptv.css">
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/socket.io.js"></script>
+    <script src="https://kit.fontawesome.com/da9aba50d5.js" crossorigin="anonymous"></script>
+
+    <title>APPTV</title>
+  </head>
+  <body>
+
+  </body>
+  
+</html>
+
+<script>
+const socket = io('http://11.11.15.8:5004');
+</script>
